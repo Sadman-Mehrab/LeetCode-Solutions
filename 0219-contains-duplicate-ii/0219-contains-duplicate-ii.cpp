e96d00e79ce85a877789a72f815e4b4505b81eb7
Sadman Mehrab <sadmannaqueeb@gmail.com>
2022-11-30 02:18:25 +0600

    Time: 281 ms (75.56%), Space: 80.6 MB (12.26%) - LeetHub

---
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        map<int,int> seen;
+        for(int i=0;i<nums.size();i++){
+            if(seen.count(nums[i]) && abs(i - seen[nums[i]]) <= k) {
+                return true;
+            }
+            seen[nums[i]] = i;
+        }
+        return false;
+    }
+};
