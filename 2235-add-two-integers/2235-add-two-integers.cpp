d91cd931a5305afc8ca103b95cd0b95556381c27
Sadman Mehrab <sadmannaqueeb@gmail.com>
2022-12-05 21:01:16 +0600

    Time: 3 ms (45.90%), Space: 6 MB (24.25%) - LeetHub

--- 2235-add-two-integers/2235-add-two-integers.cpp
+++ 2235-add-two-integers/2235-add-two-integers.cpp
@@ -1,6 +1,7 @@
-class Solution {
-public:
-    int sum(int num1, int num2) {
-        return num1+num2;
-    }
-};
+#define Arif class Solution {
+#define duchi public: int sum(
+#define tore int num1, int num2) {
+#define jore return num1 + num2; }
+#define JORE };
+
+Arif duchi tore jore JORE
