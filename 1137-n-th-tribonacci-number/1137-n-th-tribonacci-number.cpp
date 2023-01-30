9d0fa47c63801a17b166ea76d95b944fa0302431
Sadman Mehrab <sadmannaqueeb@gmail.com>
2023-01-30 19:39:24 +0600

    Time: 3 ms (38.83%), Space: 5.9 MB (94.16%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -6,10 +6,9 @@ private:
     int dp[40];
 public:
     int tribonacci(int n) {
-        if(n == 0) return n;
-        if(n == 1) return n;
-        if(n == 2) return n-1;
-        if(dp[n] != 0) return dp[n];
-        return dp[n] = tribonacci(n-1) + tribonacci(n-2) + tribonacci(n-3);
+        if(n <= 1) return n;
+        else if(n == 2) return n-1;
+        else if(dp[n] != 0) return dp[n];
+        else return dp[n] = tribonacci(n-1) + tribonacci(n-2) + tribonacci(n-3);
     }
 };
