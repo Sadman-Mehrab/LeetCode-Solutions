31fd469999bdd1438b63b809b8c68607157ec257
Sadman Mehrab <sadmannaqueeb@gmail.com>
2023-02-16 10:36:55 +0600

    Time: 156 ms (8.30%), Space: 38.6 MB (5.07%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -3,16 +3,16 @@ using namespace std;
 
 class Solution {
 public:
-    string getSorted(string a){
-        sort(a.begin(),a.end());
-        return a;
+    map <char, int> groupKey(string s){
+        map<char, int> res;
+        for(char x: s) res[x]++;
+        return res;
     }
-
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        map<string,vector<string>> groups;
+        map<map <char, int>,vector<string>> groups;
         vector<vector<string>> result;
         for(string s: strs)
-            groups[getSorted(s)].push_back(s);
+            groups[groupKey(s)].push_back(s);
         for(auto x: groups)
             result.push_back(x.second);
         return result;
