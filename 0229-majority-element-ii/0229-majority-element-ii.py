7524d11980817632e6102976a37ad18d9c474494
Sadman Mehrab <sadmannaqueeb@gmail.com>
2023-10-05 19:08:05 +0600

    Time: 105 ms (84.77%), Space: 17.8 MB (15.15%) - LeetHub

---
+++ 0229-majority-element-ii/0229-majority-element-ii.py
@@ -0,0 +1,14 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        mp = {}
+        ret = []
+        for i in nums:
+            if i in mp:
+                mp[i] += 1
+            else:
+                mp[i] = 1
+        for key in mp.keys():
+            if mp[key] > n//3:
+                ret.append(key)
+        return ret
