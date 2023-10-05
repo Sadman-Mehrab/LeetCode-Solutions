e56a8ce0bf240f6c5cf10c9b1149af7a8f6d9b58
Sadman Mehrab <sadmannaqueeb@gmail.com>
2023-10-05 20:06:38 +0600

    Time: 125 ms (9.46%), Space: 17.7 MB (49.91%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.py
+++ 0229-majority-element-ii/0229-majority-element-ii.py
@@ -1,14 +1,40 @@
+# boyer-moore majority voting algorithm    
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
-        n = len(nums)
-        mp = {}
-        ret = []
-        for i in nums:
-            if i in mp:
-                mp[i] += 1
+        c1, c2, f1, f2 = 0, 0, 0, 0
+
+        for n in nums:
+            if f1 == 0 and n != c2:
+                c1 = n
+                f1 = 1
+            elif f2 == 0 and n != c1:
+                c2 = n
+                f2 = 1
+            elif n == c1:
+                f1 += 1
+            elif n == c2:
+                f2 += 1
             else:
-                mp[i] = 1
-        for key in mp:
-            if mp[key] > n//3:
-                ret.append(key)
-        return ret
+                f1 -= 1
+                f2 -= 1
+
+        f1, f2 = 0, 0
+
+        for n in nums:
+            if n == c1:
+                f1 += 1
+            elif n == c2:
+                f2 += 1
+
+        ret = []
+
+        if f1 > len(nums)//3:
+            ret.append(c1)
+        if f2 > len(nums)//3:
+            ret.append(c2)
+        return ret 
+
+            
+                
+
+            
