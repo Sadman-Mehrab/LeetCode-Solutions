7b130356516b6483304975c74c9e18fee4e61b67
Sadman Mehrab <sadmannaqueeb@gmail.com>
2023-10-20 22:58:45 +0600

    Time: 37 ms (67.92%), Space: 16.1 MB (75.68%) - LeetHub

--- 1221-split-a-string-in-balanced-strings/1221-split-a-string-in-balanced-strings.py
+++ 1221-split-a-string-in-balanced-strings/1221-split-a-string-in-balanced-strings.py
@@ -1,14 +1,12 @@
 class Solution:
     def balancedStringSplit(self, s: str) -> int:
-        mp = {}
         res = 0
+        bal = 0
         for c in s:
-            if c not in mp:
-                mp[c] = 1
+            if c == 'R':
+                bal += 1
             else:
-                mp[c] += 1
-            if 'L' in mp and 'R' in mp:
-                if mp['L'] == mp['R']:
-                    res += 1 
-                    mp = {}
+                bal -= 1
+            if bal == 0:
+                res += 1
         return res
