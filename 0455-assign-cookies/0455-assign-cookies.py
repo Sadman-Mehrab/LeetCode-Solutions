16f9bca32cec805375f094671dd5d15edfda88af
Sadman Mehrab <sadmannaqueeb@gmail.com>
2024-01-01 21:11:02 +0600

    Time: 230 ms (11.11%), Space: 19 MB (15.85%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.py
+++ 0455-assign-cookies/0455-assign-cookies.py
@@ -3,11 +3,13 @@ class Solution:
         g.sort()
         s.sort()
         c = 0 
+        lastCookie = -1
         for n in g:
-            for i in range(len(s)):
+            for i in range(lastCookie+1,len(s)):
                 if s[i] >= n:
                     s[i] = -1
                     c += 1
+                    lastCookie = i
                     break
 
         return c 
