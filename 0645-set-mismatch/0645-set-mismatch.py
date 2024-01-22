7be746ca590f078bd69725d2a84eb9e0052da741
Sadman Mehrab <sadmannaqueeb@gmail.com>
2024-01-22 14:36:17 +0600

    Time: 208 ms (21.03%), Space: 18.5 MB (57.50%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.py
+++ 0645-set-mismatch/0645-set-mismatch.py
@@ -1,3 +1,4 @@
+import gc
 class Solution:
     def findErrorNums(self, nums: List[int]) -> List[int]:
         numSet = set()
@@ -8,6 +9,7 @@ class Solution:
             else:
                 total += n
                 numSet.add(n)
+        gc.collect()
         n = len(nums)
         missing = (n*(n+1)//2) - total
         return [dupe, missing]
