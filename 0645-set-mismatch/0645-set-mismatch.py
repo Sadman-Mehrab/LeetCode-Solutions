5b7029623205f3f58847b9d44aa134b5b3175573
Sadman Mehrab <sadmannaqueeb@gmail.com>
2024-01-22 14:31:46 +0600

    Time: 155 ms (87.84%), Space: 18.6 MB (51.72%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.py
+++ 0645-set-mismatch/0645-set-mismatch.py
@@ -1,14 +1,13 @@
 class Solution:
     def findErrorNums(self, nums: List[int]) -> List[int]:
-        dupe = 0
-        sum = 0
-        freq = defaultdict(int)
+        numSet = set()
+        dupe = total = 0
         for n in nums:
-            freq[n] += 1
-        for k in freq:
-            if freq[k] == 2:
-                dupe = k
-            sum += k
+            if n in numSet:
+                dupe = n
+            else:
+                total += n
+                numSet.add(n)
         n = len(nums)
-        missing = (n*(n+1)//2) - sum
+        missing = (n*(n+1)//2) - total
         return [dupe, missing]
