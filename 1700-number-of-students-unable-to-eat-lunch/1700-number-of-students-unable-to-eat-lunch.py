8b01e86ae6ac9b3ffb04788e731cd56abcb9ecea
Sadman Mehrab <sadmannaqueeb@gmail.com>
2024-04-08 11:49:20 +0600

    Time: 39 ms (63.55%), Space: 16.6 MB (59.90%) - LeetHub

--- 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.py
+++ 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.py
@@ -2,7 +2,7 @@ class Solution:
     def countStudents(self, students: List[int], sandwiches: List[int]) -> int:
         count = 0
         i = j = 0
-        while i < len(students) and j < len(students) and sandwiches[i] in students[j:]:
+        while j < len(students) and sandwiches[i] in students[j:]:
             if sandwiches[i] == students[j]:
                 count += 1
                 i += 1
